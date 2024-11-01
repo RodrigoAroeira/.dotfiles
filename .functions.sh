650857a5696ee3760100544df71a7f8d2cb199c8
RodrigoAroeira <raroeirabr@gmail.com>
2024-11-01 10:51:32 -0300

    feat: Added functions that create templates for .clangd, CMakeLists and Makefile

--- .functions.sh
+++ .functions.sh
@@ -11,15 +11,17 @@ function devdir() {
 }
 
 _devdir() {
-  local cur prev words cword
+  local cur
   _init_completion || return
-  local completions=($(cd ~/Dev/ && compgen -o dirnames -- "$cur"))
+  local completions
+  mapfile -t completions < <(cd ~/Dev/ && compgen -o dirnames -- "$cur")
   if [[ ${#completions[@]} -gt 0 ]]; then
     COMPREPLY=("${completions[@]}/")
   fi
 }
 
 complete -o nospace -F _devdir devdir
+complete -o nospace -F _devdir mkdev
 
 export devdir="$HOME/Dev/"
 
@@ -67,7 +69,7 @@ function _setup_daily_readme() {
   local file_path="$1"
   local challenge="$2"
   if [ ! -f "$file_path" ]; then
-    echo -e "TODAY'S TASK: $2\n" >"$file_path"
+    echo -e "# TODAY'S TASK: $2\n" >"$file_path"
     echo -e "STARTING:\n" >>"$file_path"
     echo -e "ENDED:\n" >>"$file_path"
   fi
@@ -79,7 +81,7 @@ daily_repo="$devdir"/DailyChallenges
 function start-daily() {
   if [ "$DAILY_CHALLENGE_STARTED" = true ]; then
     echo "Daily Challenge already started."
-    cd "$daily_folder"
+    cd "$daily_folder" || return
     return
   fi
   local file=README.md
@@ -125,8 +127,87 @@ function end-daily() {
     return 1
   fi
 
+  cd "$folder_in_repo"/.. || return
+  gh repo create
+  cd "$folder_in_repo" || return
+
   mv "$daily_folder"/* "$folder_in_repo"
   rm -r "$daily_folder"
 
   unset DAILY_CHALLENGE_STARTED
+}
+
+function setupBasicClangd() {
+  local filename=".clangd"
+  cat <<EOF >"$filename"
+CompileFlags:
+  Add:
+    - -I$(pwd)/headers
+    - -I$(pwd)/includes/usr/include
+EOF
+}
+
+function setupBasicMakefile() {
+  local filename="Makefile.test"
+  local projectName=$1
+  function tab() {
+    printf '\t'
+  }
+  cat <<EOF >"$filename"
+CXX = g++
+
+STD = c++11
+
+SRC_DIR = src
+SRC_FILES = \$(wildcard \$(SRC_DIR)/*.cpp)
+
+HEADER_DIR = headers
+INCLUDE_DIR = includes/usr/include
+
+OUT = $projectName
+
+CXXFLAGS = -I\$(HEADER_DIR) -O3 -std=\$(STD)
+
+build:
+$(tab)\$(CXX) \$(SRC_FILES) \$(CPPFLAGS) -o \$(OUT)
+
+run:
+$(tab)./\$(OUT)
+
+clean:
+$(tab)rm -f \$(OUT)
+
+.PHONY: build run clean
+EOF
+}
+
+function setupBasicCMake() {
+  local filename="CMakeLists.txt"
+  local projectName=${1:-"PLACEHOLDER"}
+
+  cat <<EOF >"$filename"
+cmake_minimum_required(VERSION 3.10)
+
+set(PROJECT_NAME "$projectName")
+
+project("\${PROJECT_NAME}" VERSION 1.0)
+
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+
+set(SRC_DIR "src")
+file(GLOB SRC_FILES "\${SRC_DIR}/*.cpp")
+
+add_executable(\${PROJECT_NAME} \${SRC_FILES})
+
+target_include_directories(\${PROJECT_NAME} PRIVATE headers includes/usr/include)
+target_compile_options(\${PROJECT_NAME} PRIVATE -O3)
+
+
+add_custom_target(run
+COMMAND \${PROJECT_NAME}
+DEPENDS \${PROJECT_NAME}
+WORKING_DIRECTORY \${CMAKE_PROJECT_DIR}
+)
+EOF
 }
