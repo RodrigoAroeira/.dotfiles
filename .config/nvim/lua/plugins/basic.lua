3f484b7d78ea80ceeb8be5531dd57b9fadb26f7b
RodrigoAroeira <raroeirabr@gmail.com>
2024-11-27 01:59:35 -0300

    refactor: removed custom lualine opts

--- .config/nvim/lua/plugins/basic.lua
+++ .config/nvim/lua/plugins/basic.lua
@@ -4,23 +4,6 @@ local loadGruvbox = { "LazyVim/LazyVim", opts = {
   colorscheme = "gruvbox",
 } }
 
-local lualine_opts = function()
-  return {
-    --[[add your custom lualine config here]]
-    icons_enabled = false,
-  }
-end
+local surround = { "tpope/vim-surround", lazy = false }
 
-local lualine = {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  opts = lualine_opts,
-  -- enabled = false,
-  options = {
-    icons_enabled = false,
-  },
-}
-
-local surround = { "tpope/vim-surround" }
-
-return { gruvbox, loadGruvbox, lualine, surround }
+return { gruvbox, loadGruvbox, surround }
