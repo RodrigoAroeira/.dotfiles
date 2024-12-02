12d7558de2ff824ab151a572c7747a18ea07f542
RodrigoAroeira <raroeirabr@gmail.com>
2024-12-02 10:15:39 -0300

    fix: Changed sourced functions file when using bash

--- .globalrc.sh
+++ .globalrc.sh
@@ -1,12 +1,12 @@
-if [ -n "$ZSH_VERSION" ]; then
-  source ~/.functions.zsh
-elif [ -n "$BASH_VERSION" ]; then
-  source ~/.bashfunctions.sh
-fi
-
 export devdir="$HOME/Dev"
 
-source ~/.functions.sh
+source "$HOME"/.functions.sh
+
+if [ -n "$ZSH_VERSION" ]; then
+  source "$HOME"/.functions.zsh
+elif [ -n "$BASH_VERSION" ]; then
+  source "$HOME"/.functions.bash
+fi
 
 alias .=source
 
@@ -33,7 +33,7 @@ export ia01="ia01.LCC.ufmg.br"
 alias ia02='ssh rodrigo-aroeira@ia02.LCC.ufmg.br'
 export ia02="ia02.LCC.ufmg.br"
 
-alias dotf='cd ~/.dotfiles/'
+alias dotf='cd "$HOME"/.dotfiles/'
 
 alias dotfn='dotf && cd .config/nvim'
 
@@ -44,3 +44,7 @@ alias fp='ps -eaf | grep'
 alias ssh="kitten ssh"
 
 alias vim="nvim --clean"
+
+alias vag='nvim "$HOME"/.globalrc.sh && src && echo "Updated Global Aliases" '
+
+alias vfg='nvim "$HOME"/.functions.sh && src && echo "Updated Global Functions" '
