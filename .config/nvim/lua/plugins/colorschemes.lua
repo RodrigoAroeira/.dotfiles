d035245fbd9f68136d92fc52ebecb3ee98ce18d6
RodrigoAroeira <raroeirabr@gmail.com>
2025-09-29 19:37:44 -0300

    removed nvim config

--- .config/nvim/lua/plugins/colorschemes.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "blazkowolf/gruber-darker.nvim" }, -- Tsoding theme
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
-  { "ellisonleao/gruvbox.nvim" },
-  { "folke/tokyonight.nvim" },
-  { "Mofiqul/vscode.nvim" },
-  { "shaunsingh/nord.nvim" },
-  { "neanias/everforest-nvim" },
-}
--- .config/nvim/lua/plugins/snacks.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "folke/snacks.nvim",
-  opts = { explorer = { replace_netrw = false } },
-}
